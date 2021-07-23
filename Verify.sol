59372653f1b5dd9b3a132d208e1552fb9f0319f6
Christoph Michelbach <michelbach94@gmail.com>
2021-07-23 11:39:29 +0200

    Port to Solidity 0.8.

--- Verify.sol
+++ Verify.sol
@@ -1,11 +1,10 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.1;
 
 /*
-License: GPL-3.0
 Author: Christoph Michelbach
 Contact: christoph.michelbach@blockinfinity.com
 */
-
 import "./EccOperations/EccOperations.sol";
 
 library Verify {
